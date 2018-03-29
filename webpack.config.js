cdc2a2f8b9b1479c148b3bc9a8ed1a34999a0490
Willy Liu <arkazain@gmail.com>
2018-03-29 17:40:31 +0200

    🐛 Fix name of import

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ const path = require("path");
 const webpack = require("webpack");
 const merge = require("webpack-merge");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
-const ExtractTextPlugin = require("mini-css-extract-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
