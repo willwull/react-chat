b66c5bd438794d5ff93df459f0316b5eb1929a83
Willy Liu <arkazain@gmail.com>
2018-03-31 13:25:44 +0200

    ✅ Add name input

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,29 @@ import ChatPanel from "./ChatPanel";
 
 class App extends React.Component {
   state = {
-    username: "Bob",
+    username: "",
+  }
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    const inputVal = e.target.username.value;
+    if (!inputVal) return;
+
+    this.setState({ username: inputVal });
   }
 
   render() {
+    const { username } = this.state;
+    if (!username) {
+      return (
+        <div>
+          <form onSubmit={this.onFormSubmit}>
+            <input type="text" name="username" id="username" />
+            <button type="submit">Set name</button>
+          </form>
+        </div>
+      );
+    }
     return (
       <ChatPanel username={this.state.username} />
     );
