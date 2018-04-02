3fb9b571040b59bce71470e4b916e52c6170a522
Willy Liu <arkazain@gmail.com>
2018-04-02 23:14:21 +0200

    ✅ Add swal

--- src/components/Threads.js
+++ src/components/Threads.js
@@ -1,14 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
+import swal from "sweetalert2";
 import ThreadItem from "./ThreadItem";
 import "../stylesheets/Threads.scss";
 
 function Threads(props) {
+  const clickHandler = () => {
+    swal({
+      text: "Enter a name",
+      input: "text",
+      showCancelButton: true,
+      confirmButtonText: "Create conversation",
+      reverseButtons: true,
+      customClass: "thread-popup",
+      cancelButtonClass: "cancel",
+      confirmButtonClass: "confirm",
+      buttonsStyling: false,
+    });
+  };
+
   return (
     <div className="threads">
       <div className="titlebar">
         Conversations
-        <button id="new-convo-btn">
+        <button id="new-convo-btn" onClick={clickHandler}>
           New
         </button>
       </div>
